016338f2bceb67641736121c4c6801bb672ffb21
rf y <709241197@qq.com>
2019-11-08 02:53:33 +0800

    01

---
+++ client/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => <div>This is a App!!!</div>;
+
+// export default class App extends React.Component {
+//   render() {
+//     return <div>{'This is a App!!!'}</div>;
+//   }
+// }
